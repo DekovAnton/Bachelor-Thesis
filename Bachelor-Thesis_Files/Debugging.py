5c173eea46c80bd5996d4e8a506567e0b4871273
Dekov <a.a.dekov@gmail.com>
2024-07-29 20:37:35 +0300

    removed files from subdirectory

--- Bachelor-Thesis_Files/Debugging.py
+++
@@ -1,6 +0,0 @@
-import pyvisa
-
-resource_manager= pyvisa.ResourceManager()
-Serial_COM_ports=resource_manager.list_resources()
-Instrument=resource_manager.open_resource("Name of instrument that will be used for data acquistion")
-Instrument.query("MEASure:VOLTage:DC?")
